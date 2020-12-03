f25a9671c9bd131024df88fc44c5d693720874c8
Rose Kitz <rose.kitz@tufts.edu>
2020-12-03 13:20:49 -0500

    added existing keys to new code

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "xr7VbnrJcT601p";     // fill in your info
+var airtable_baseid = "app" + "lB5sA1RQTWWwdf"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
